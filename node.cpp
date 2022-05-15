c14286c7254c1b0800961ecff1e9fea80c971942
南山二毛 <30722103+MRwangmaomao@users.noreply.github.com>
2022-05-16 00:17:38 +0800

    [修改内容] 反光板检测算法 [遗留问题] 激光反光板ICP匹配算法或三点定位算法 [修改人] 南山二毛

--- node.cpp
+++ node.cpp
@@ -7,19 +7,31 @@
 #include <iostream>
 #include <vector>
 
+const int THRETHOLD = 1000;
+const int WINDOWS_SIZE = 80;
+
+
 void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
-    // Get laser data.
+    // Get laser data. frame_id is scanner
     ROS_INFO("get laser seq is %d", msg->header.seq);
     std::vector<float> ranges = msg->ranges;
-
-    for (int i = 0; i < ranges.size(); i++) {
-        double angle = msg->angle_min + i * msg->angle_increment;
-        double point_x = ranges[i] * cos(angle);
-        double point_y = ranges[i] * sin(angle);
-        float intensity = msg->intensities[i];
-        
-        // std::cout << ranges[i] << "," << std::endl;
+    for (int i = WINDOWS_SIZE - 1; i < ranges.size() - WINDOWS_SIZE - 1; i++) {
+        if (msg->intensities[i] > THRETHOLD && (msg->intensities[i] > msg->intensities[i-1]
+            && msg->intensities[i] > msg->intensities[i+1])) {
+            int max = 0;
+            for(int j = i - WINDOWS_SIZE; j < i + WINDOWS_SIZE; j++) {
+                if (max < msg->intensities[j]) {
+                    max = msg->intensities[j];
+                }
+            }
+            if (msg->intensities[i] >= max) {
+                double angle = msg->angle_min + i * msg->angle_increment;
+                double point_x = ranges[i] * cos(angle);
+                double point_y = ranges[i] * sin(angle);
+                ROS_INFO("      detect reflect position is %f, %f", point_x, point_y);
+            }
+        }
     }
 
 }
